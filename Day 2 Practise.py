8fa2c4ff979f67c3ab488b046ccf2a0b8d91add5
Priyarajbhar25 <priyarajbhar7773@gmail.com>
2025-01-16 13:33:33 +0530

    Add files via upload

---
+++ Day 2 Practise.py
@@ -0,0 +1,50 @@
+# Define two numbers
+number1 = int(input("Enter 1st Number: "))  
+number2 = int(input("Enter 2nd Number: "))  
+
+# Calculate multiplication and sum
+multiplication = number1 * number2
+sum = number1 + number2 
+
+# Print results
+print("Multiplication is:", multiplication)
+print("Sum is:", sum)
+
+
+
+
+# Declare two variables
+Variable_A = int(input("Enter 1st Number: "))
+Variable_B = int(input("Enter 2nd Number: "))
+int(input("Enter 1st Number: "))  
+ 
+# Find and print the largest variable using a ternary operator
+largest = Variable_A if Variable_A > Variable_B else Variable_B
+print("The largest variable is:", largest)
+
+
+
+
+
+# Input temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Print the result
+print(str(celsius) + "°C is equal to " + str(fahrenheit) + "°F")
+
+
+
+
+
+#Define Breadth and Height 
+Breadth =float(input("Enter Breadth"))
+Height =float(input("Enter eight"))
+
+# Calculate Area
+Area = 1/2(Breadth * Height)
+
+# Print results
+print("Area of Triangle is:", Area)
